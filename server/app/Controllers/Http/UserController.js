77acc7a926eef3b22f05841897b03b30e5027469
Xu Zewei <xuzewei@gmail.com>
2018-12-30 07:56:50 +0800

    Add api/v0/register function.

---
+++ server/app/Controllers/Http/UserController.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class UserController {
+  async register({ request }) {
+    const { email, password } = request.all();
+    const user = await User.create({
+      email,
+      password,
+      username: email,
+    });
+    return user;
+  }
+}
+
+module.exports = UserController
